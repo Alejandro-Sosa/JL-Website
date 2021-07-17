e43dc09cae079f821cc5c5bcf075411ba1b76021
Thomas Johnson <psychojohnson666@gmail.com>
2021-07-17 16:12:20 +0200

    Progresss

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyASEQ6EAjsnn0e-Hniq7za7rBxuxspTH_Y",
+    authDomain: "website-johnson.firebaseapp.com",
+    projectId: "website-johnson",
+    storageBucket: "website-johnson.appspot.com",
+    messagingSenderId: "461520491126",
+    appId: "1:461520491126:web:f817920948046ef87d8557",
+    measurementId: "G-5HZHLFWJPB"
+  }
 };
